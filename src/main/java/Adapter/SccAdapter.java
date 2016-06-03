278f903ec4da76b2464119c9c9b7b4fbfd480df8
Matias L Ceballos <m.lionel.tkd@gmail.com>
2016-06-03 19:07:07 -0300

    Implementada clase adapter

--- src/main/java/Adapter/SccAdapter.java
+++ src/main/java/Adapter/SccAdapter.java
@@ -15,56 +15,56 @@ public class SccAdapter implements BeatModelInterface {
 
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
+		scc.initialize();
 
 	}
 
 	@Override
 	public void on() {
 		// TODO Auto-generated method stub
-
+		scc.on();
 	}
 
 	@Override
 	public void off() {
 		// TODO Auto-generated method stub
-
+		scc.off();
 	}
 
 	@Override
 	public void setBPM(int bpm) {
 		// TODO Auto-generated method stub
-
+		scc.setSpeed(bpm);
 	}
 
 	@Override
 	public int getBPM() {
 		// TODO Auto-generated method stub
-		return 0;
+		return scc.getSpeed();
 	}
 
 	@Override
 	public void registerObserver(BeatObserver o) {
 		// TODO Auto-generated method stub
-
+		scc.registerObserver(o);
 	}
 
 	@Override
 	public void removeObserver(BeatObserver o) {
 		// TODO Auto-generated method stub
-
+		scc.removeObserver(o);
 	}
 
 	@Override
 	public void registerObserver(BPMObserver o) {
 		// TODO Auto-generated method stub
-
+		scc.registerObserver(o);
 	}
 
 	@Override
 	public void removeObserver(BPMObserver o) {
 		// TODO Auto-generated method stub
-
+		scc.removeObserver(o);
 	}
 
 }
