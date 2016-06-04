83c6eb545488d35eb852930e58d3e03b7a194f27
Gaspyarmando <gasparmando@hotmail.com>
2016-06-04 03:56:08 -0300

    Nueva implementacion de modelo con clase Regulador
    La regulacion de velocidad se realiza paralelamente para no atrasar las tareas del modelo

---
+++ src/main/java/Class/Regulador.java
@@ -0,0 +1,35 @@
+package main.java.Class;
+
+import main.java.Model.SccModel;
+
+public class Regulador implements Runnable {
+	SccModel model;
+	
+	public Regulador(SccModel model){
+		this.model = model;
+	}
+		
+		
+
+	@Override
+	public void run(){
+		while(model.getSpeed()>=1){
+			try {
+				Thread.sleep(100);
+				if(model.getSpeed()>model.getTargetSpeed())
+					model.modifyCurrentSpeed(-1);
+				else
+					model.modifyCurrentSpeed(1);
+				model.notifyBPMObservers();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		
+			
+		}
+		
+		
+
+	}
+
+}
