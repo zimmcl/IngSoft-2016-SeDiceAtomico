b908f7039abf12f05e2c09d52f06d816f6fc6382
Gaspyarmando <gasparmando@hotmail.com>
2016-06-09 02:24:02 -0300

    Agrego imagenes de animacion al repositorio

--- src/main/java/TestDrive/SccTestDrive.java
+++ src/main/java/TestDrive/SccTestDrive.java
@@ -9,7 +9,7 @@ public class SccTestDrive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SccModel sccModel = new SccModel();
-        ControllerInterface model = new SccController(sccModel, 1 ); 
+        ControllerInterface model = new SccController(sccModel, true); 
 		
 	}
 		
